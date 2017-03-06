f65ad256054ba3eea035798ec5b91123399d9d82
jfu006 <jfu006@ucr.edu>
2017-03-05 21:02:04 -0800

    Integrated GPS code, outputs latitude coordinates

--- Tracker/transmitter.ino
+++ Tracker/transmitter.ino
@@ -1,4 +1,3 @@
-
 /*
 * Getting Started example sketch for nRF24L01+ radios
 * This is an example of how to send data from one node to another using data structures
@@ -7,6 +6,34 @@
 
 #include <SPI.h>
 #include "RF24.h"
+#include <Adafruit_GPS.h>
+#include <SoftwareSerial.h>
+
+//*************************GPS*************************
+// Connect the GPS Power pin to 5V
+// Connect the GPS Ground pin to ground
+// If using software serial (sketch example default):
+//   Connect the GPS TX (transmit) pin to Digital 3
+//   Connect the GPS RX (receive) pin to Digital 2
+// If using hardware serial (e.g. Arduino Mega):
+//   Connect the GPS TX (transmit) pin to Arduino RX1, RX2 or RX3
+//   Connect the GPS RX (receive) pin to matching TX1, TX2 or TX3
+
+// If using software serial, keep this line enabled
+// (you can change the pin numbers to match your wiring):
+SoftwareSerial mySerial(3, 2);
+
+Adafruit_GPS GPS(&mySerial);
+
+// Set GPSECHO to 'false' to turn off echoing the GPS data to the Serial console
+// Set to 'true' if you want to debug and listen to the raw GPS sentences. 
+#define GPSECHO  true
+
+// this keeps track of whether we're using the interrupt
+// off by default!
+boolean usingInterrupt = false;
+void useInterrupt(boolean);
+//*************************GPS*************************
 
 byte addresses[][6] = {"1Node","2Node"};
 
@@ -38,6 +65,30 @@ void setup() {
   Serial.begin(115200);
   Serial.println(F("RF24/examples/GettingStarted_HandlingData"));
   Serial.println(F("*** PRESS 'T' to begin transmitting to the other node"));
+
+//*************************GPS*************************
+  // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
+  GPS.begin(9600);
+  
+  // turn on RMC (recommended minimum) and GGA (fix data) including altitude
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+
+  // Set the update rate
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
+
+  // Request updates on antenna status, comment out to keep quiet
+  GPS.sendCommand(PGCMD_ANTENNA);
+
+  // the nice thing about this code is you can have a timer0 interrupt go off
+  // every 1 millisecond, and read data from the GPS for you. that makes the
+  // loop code a heck of a lot easier!
+  useInterrupt(true);
+
+  delay(1000);
+  
+  // Ask for firmware version
+  mySerial.println(PMTK_Q_RELEASE);
+//*************************GPS*************************
   
   radio.begin();
 
@@ -54,17 +105,103 @@ void setup() {
     radio.openReadingPipe(1,addresses[1]);
   }
   
-  myData.value = 333;
+  //myData.value = 333;
   // Start the radio listening for data
   radio.startListening();
 }
 
+// Interrupt is called once a millisecond, looks for any new GPS data, and stores it
+SIGNAL(TIMER0_COMPA_vect) {
+  char c = GPS.read();
+  // if you want to debug, this is a good time to do it!
+#ifdef UDR0
+  if (GPSECHO)
+    if (c) UDR0 = c;  
+    // writing direct to UDR0 is much much faster than Serial.print 
+    // but only one character can be written at a time. 
+#endif
+}
 
+void useInterrupt(boolean v) {
+  if (v) {
+    // Timer0 is already used for millis() - we'll just interrupt somewhere
+    // in the middle and call the "Compare A" function above
+    OCR0A = 0xAF;
+    TIMSK0 |= _BV(OCIE0A);
+    usingInterrupt = true;
+  } else {
+    // do not call the interrupt function COMPA anymore
+    TIMSK0 &= ~_BV(OCIE0A);
+    usingInterrupt = false;
+  }
+}
+
+uint32_t timer = millis();
 
 
 void loop() {
+
+//*************************GPS*************************
+if (! usingInterrupt) {
+    // read data from the GPS in the 'main loop'
+    char c = GPS.read();
+    // if you want to debug, this is a good time to do it!
+    if (GPSECHO)
+      if (c) Serial.print(c);
+  }
   
+  // if a sentence is received, we can check the checksum, parse it...
+  if (GPS.newNMEAreceived()) {
+    // a tricky thing here is if we print the NMEA sentence, or data
+    // we end up not listening and catching other sentences! 
+    // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
+    //Serial.println(GPS.lastNMEA());   // this also sets the newNMEAreceived() flag to false
   
+    if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
+      return;  // we can fail to parse a sentence in which case we should just wait for another
+  }
+
+  // if millis() or timer wraps around, we'll just reset it
+  if (timer > millis())  timer = millis();
+
+  // approximately every 2 seconds or so, print out the current stats
+  if (millis() - timer > 2000) { 
+    timer = millis(); // reset the timer
+    
+    Serial.print("\nTime: ");
+    Serial.print(GPS.hour, DEC); Serial.print(':');
+    Serial.print(GPS.minute, DEC); Serial.print(':');
+    Serial.print(GPS.seconds, DEC); Serial.print('.');
+    Serial.println(GPS.milliseconds);
+    Serial.print("Date: ");
+    Serial.print(GPS.day, DEC); Serial.print('/');
+    Serial.print(GPS.month, DEC); Serial.print("/20");
+    Serial.println(GPS.year, DEC);
+    Serial.print("Fix: "); Serial.print((int)GPS.fix);
+    Serial.print(" quality: "); Serial.println((int)GPS.fixquality); 
+    if (GPS.fix) {
+      Serial.print("Location: ");
+      Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
+      Serial.print(", "); 
+      Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
+      Serial.print("Location (in degrees, works with Google Maps): ");
+      Serial.print(GPS.latitudeDegrees, 4);
+      Serial.print(", "); 
+      Serial.println(GPS.longitudeDegrees, 4);
+      
+      Serial.print("Speed (knots): "); Serial.println(GPS.speed);
+      Serial.print("Angle: "); Serial.println(GPS.angle);
+      Serial.print("Altitude: "); Serial.println(GPS.altitude);
+      Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
+    }
+  }
+
+
+  myData.value = GPS.latitudeDegrees;
+  //myData.value += GPS.longitudeDegrees;
+
+//*************************GPS*************************
+   
 /****************** Ping Out Role ***************************/  
 if (role == 1)  {
     
@@ -111,33 +248,6 @@ if (role == 1)  {
     // Try again 1s later
     delay(1000);
   }
-
-
-
-///****************** Pong Back Role ***************************/
-//
-//  if ( role == 0 )
-//  {
-//    
-//    if( radio.available()){
-//                                                           // Variable for the received timestamp
-//      while (radio.available()) {                          // While there is data ready
-//        radio.read( &myData, sizeof(myData) );             // Get the payload
-//      }
-//     
-//      radio.stopListening();                               // First, stop listening so we can talk  
-//      myData.value += 0.01;                                // Increment the float value
-//      radio.write( &myData, sizeof(myData) );              // Send the final one back.      
-//      radio.startListening();                              // Now, resume listening so we catch the next packets.     
-//      Serial.print(F("Sent response "));
-//      Serial.print(myData._micros);  
-//      Serial.print(F(" : "));
-//      Serial.println(myData.value);
-//   }
-// }
-
-
-
 
 /****************** Change Roles via Serial Commands ***************************/
 
